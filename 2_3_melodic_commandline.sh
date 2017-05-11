6e197e6fad59cfaf011e16fa988f0d0e0d0555ec
kcho <sky8671@gmail.com>
2017-05-11 14:11:38 +0900

    melodic command line loop

--- 2_3_melodic_commandline.sh
+++ 2_3_melodic_commandline.sh
@@ -1,6 +1,7 @@
-melodic -i list_melodic.txt -o commandLine_3ds_10  -v --nobet --bgthreshold=10 --mmthresh=0.5 --tr=1 --report -d 10 --Ostats -a concat
-melodic -i list_melodic.txt -o commandLine_3ds_20  -v --nobet --bgthreshold=10 --mmthresh=0.5 --tr=1 --report -d 20 --Ostats -a concat
+# need comment on list_melodic.txt
+for dLevel in 10 20
+do
+    melodic -i list_melodic.txt -o commandLine_3ds_${dLevel} -v --nobet --bgthreshold=10 --mmthresh=0.5 --tr=1 --report -d ${dLevel} --Ostats -a concat
 
-
-melodic -i list_header_edit_melodic.txt -o commandLine_3ds_hdEdit_10  -v --nobet --bgthreshold=10 --mmthresh=0.5  --tr=3.50 --report -d 10 --Ostats -a concat
-melodic -i list_header_edit_melodic.txt -o commandLine_3ds_hdEdit_20  -v --nobet --bgthreshold=10 --mmthresh=0.5  --tr=3.50 --report -d 20 --Ostats -a concat
+    melodic -i list_header_edit_melodic.txt -o commandLine_3ds_hdEdit_${dLevel}  -v --nobet --bgthreshold=10 --mmthresh=0.5  --tr=3.50 --report -d ${dLevel} --Ostats -a concat
+done
