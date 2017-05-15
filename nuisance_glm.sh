b15e1075ea6b3e0b27f99522dc3e8c5e24522774
Yoon Ji Lee <ylee17@wellesley.edu>
2017-05-15 15:27:18 +0900

    added comments

--- nuisance_glm.sh
+++ nuisance_glm.sh
@@ -1,8 +1,11 @@
+# for every C/F/N* subject in current directory
 for i in [CNF]*
 do
 
+# merge demeaned_CSF_noise and demeaned_WM_noise into a textfile with two columns
 paste ${i}/nuisance/demeaned_CSF_noise.txt ${i}/nuisance/demeaned_WM_noise.txt > ${i}/nuisance/design.txt
 
+# convert textfiles into the format used by FSL tools
 Text2Vest ${i}/nuisance/design.txt ${i}/nuisance/design.mat
 
 Text2Vest ${i}/nuisance/contrast.txt ${i}/nuisance/design.con
--- nuisance_matrix.py
+++ nuisance_matrix.py
@@ -3,27 +3,31 @@
 import os
 import numpy as np
 
-
+#for every C/F/N* subject in current directory
 for subject in os.listdir(os.getcwd()):
     if subject.startswith(('C','F','N')):
         matrices = 'CSF_noise.txt', 'WM_noise.txt', 'mn.txt'
         for matrix in matrices:
+            # file location settings - 'noise' files under 'nuisance' folder
             if matrix.startswith(('C', 'W')):
                 nuisance_path = '{subject}/nuisance'.format(subject=subject)
                 pre = np.loadtxt('{subject}/nuisance/{matrix}'.format(subject=subject, matrix=matrix))
+                # demean = remove the mean of each column of a matrix
                 demean = pre - pre.mean()
                 np.savetxt(os.path.join(nuisance_path, 'demeaned_{matrix}').format(matrix=matrix), demean)
-
+                # create contrast matrix
                 nuisance_contrast = np.asmatrix('1 0')
                 np.savetxt(os.path.join(nuisance_path, 'contrast.txt'), nuisance_contrast)
 
+            # file location settings - 'mn' file under 'motion' folder
             if matrix.startswith('m'):
                 mn_path = '{subject}/motion'.format(subject=subject)
                 mn = np.loadtxt('{subject}/motion/{matrix}'.format(subject=subject, matrix=matrix))
+                # delete row 114,115 and demean
                 mn_delete = np.delete(mn, (114,115), 0)
                 mn_demean = mn_delete - mn_delete.mean()
                 np.savetxt(os.path.join(mn_path, 'demeaned_{matrix}').format(matrix=matrix), mn_demean)
-
+                # create contrast matrix
                 mn_contrast = np.asmatrix('1')
                 np.savetxt(os.path.join(mn_path, 'contrast.txt'), mn_contrast)
 
