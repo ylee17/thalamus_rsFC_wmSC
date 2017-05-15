6072db7e454066717e6b3470d7276b376d5353fb
Yoon Ji Lee <ylee17@wellesley.edu>
2017-05-15 18:57:35 +0900

    added comments

--- header_edit.sh
+++ header_edit.sh
@@ -1,6 +1,8 @@
+# for every C/F/N* subject in current directory
 for i in [CFN]*
 do
 
+    # copy 'edit' nifti file and edit header to text file
     cp -r ${i}/downsampled/fisherZ_3ds.nii.gz ${i}/downsampled/edit_fisherZ_3ds.nii.gz
     fslhd -x ${i}/downsampled/edit_fisherZ_3ds.nii.gz > ${i}/downsampled/edit_fisherZ_3ds.txt
 
@@ -10,11 +12,12 @@ do
     cp -r ${i}/downsampled/fisherZ_5ds.nii.gz ${i}/downsampled/edit_fisherZ_5ds.nii.gz
     fslhd -x ${i}/downsampled/edit_fisherZ_5ds.nii.gz > ${i}/downsampled/edit_fisherZ_5ds.txt
 
+    # string substitutions
     sed -i -e "s/dt = '1'/dt = '3.50'/g" ${i}/downsampled/edit_fisherZ_3ds.txt
     sed -i -e "s/dt = '1'/dt = '3.50'/g" ${i}/downsampled/edit_fisherZ_4ds.txt
     sed -i -e "s/dt = '1'/dt = '3.50'/g" ${i}/downsampled/edit_fisherZ_5ds.txt
 
-
+    # header back to nifti
     fslcreatehd ${i}/downsampled/edit_fisherZ_3ds.txt ${i}/downsampled/edit_fisherZ_3ds.nii.gz
     fslcreatehd ${i}/downsampled/edit_fisherZ_4ds.txt ${i}/downsampled/edit_fisherZ_4ds.nii.gz
     fslcreatehd ${i}/downsampled/edit_fisherZ_5ds.txt ${i}/downsampled/edit_fisherZ_5ds.nii.gz
